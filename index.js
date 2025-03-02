81356431ec0d847141d43177d5671d1787d8f814
AnnaMariaSch <anna-maria-schmitt@gmx.de>
2025-03-02 07:21:02 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function showAnswer(response) {
+  alert(response.data.answer);
+}
+
+let apiKey = "b8bac409foc24c511t14abf5300b7242";
+let prompt = "What is the capital city of Spain";
+let context = "Make sure to provide a clear and precise answer";
+let apiUrl =
+  "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
+
+axios.get(apiUrl).then(showAnswer);
+console.log(hi);
