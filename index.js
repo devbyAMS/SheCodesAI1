c4d41bc1fe9c535397ecac5bd4a9d90a19ba2c0c
AnnaMariaSch <anna-maria-schmitt@gmx.de>
2025-03-02 07:31:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function showAnswer(response) {
 let apiKey = "b8bac409foc24c511t14abf5300b7242";
 let prompt = "What is the capital city of Spain";
 let context = "Make sure to provide a clear and precise answer";
-let apiUrl =
-  "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(apiUrl).then(showAnswer);
 console.log(hi);
